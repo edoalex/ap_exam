8217d73c5fabc7dfdc91cbf9f1896555a4c91ff8
Edoardo Alessandroni <edo-alex@hotmail.com>
2020-02-10 12:34:04 +0100

    seg fault

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 
 int main(){
 	bst<int,int> tree{};
-	tree.insert({5,8});
+	tree.insert({7,8});
 	tree.insert({2,2});
-	tree.insert({3,4});
+	tree.insert({8,4});
 	tree.insert({1,1});
-	tree.insert({4,3});
-	tree.insert({6,1});
+	tree.insert({5,3});
+	tree.insert({4,1});
+	tree.insert({6,3});
+	tree.insert({3,1});
 	/*tree.insert({5,2});
 	tree.insert({7,1});
 	tree.insert({12,12});
@@ -21,7 +23,7 @@ int main(){
 	*/
 	std::cout << tree << std::endl;
 
-	tree.erase(3);
+	tree.erase(2);
 
 	std::cout << tree << std::endl;
 	
